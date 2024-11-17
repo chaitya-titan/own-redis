ad08e4324b1dfa2fd0cc15396841c50820e2c93e
Chaitya W <chaityaw@Chaityas-MacBook-Air.local>
2024-11-17 17:31:11 +0530

    Echo Command

--- src/main/java/services/HandleClientService.java
+++ src/main/java/services/HandleClientService.java
@@ -4,9 +4,11 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.Socket;
+import java.util.ArrayList;
 
 public class HandleClientService {
     public void handleClient(Socket clientSocket){
+        ArrayList<ArrayList<String>> arr = new ArrayList<>();
         try {
             BufferedReader input = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
             while (true) {
@@ -14,6 +16,12 @@ public class HandleClientService {
                 System.out.println(line);
                 if (line.equalsIgnoreCase("ping")) {
                     clientSocket.getOutputStream().write("+PONG\r\n".getBytes());
+                }else if(line.equalsIgnoreCase("echo")){
+                    input.readLine();
+                    String data = input.readLine();
+                    clientSocket.getOutputStream().write(
+                            String.format("$%d\r\n%s\r\n", data.length(), data)
+                                    .getBytes());
                 }
             }
         } catch (IOException e){
